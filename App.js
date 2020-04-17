a998edf49a9f1b8514a992262a146bcae4383833
Nadia Andrushchenko <nadya.andrushenko@gmail.com>
2020-04-16 22:34:15 -0300

    updates

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React, {Component}from 'react';
-import {ScrollView,
+import {StyleSheet,
+        //ScrollView,
         FlatList,
+        ActivityIndicator,
         View} from 'react-native';
-import Card from './components/Card/Card';      
-import Filter from './components/Filter/Filter'; 
-import { styles } from './components/styles';
+import Card from './components/Card/Card';        
 
 export default class App extends Component { 
 
   constructor() {
     super();
     this.state = {
-      items: []     
+      items: []
     }
   }
 
-  componentDidMount() {
+componentDidMount() {
      this._get('http://www.mocky.io/v2/5e8bbc982f00006d0088c4ed').then(
        data => {
          this.setState({items: data})
@@ -23,43 +23,43 @@ export default class App extends Component {
     )
 }
 
-
-_get = async (endpoint) => {
-  const res = await fetch(endpoint);
-  const data = await res.json();
-  return data;
-  }
- 
-
  render() {
    
-    /* if(this.state.items.length===0) {
+  /*return (
+    <View><Text>Filtro</Text></View>
+  );*/
+
+
+    if(this.state.items.length===0) {
       return (
         <View style={styles.loader}>
           <ActivityIndicator size="large" />
         </View>
       );
-    }*/
+    }
   
    return (
-
-    <View> 
-
-    <Filter /> 
-    
-    <View>
-     <ScrollView>
-      <FlatList 
+     <FlatList 
        style={styles.container}
        data={this.state.items}
        keyExtractor={(item, index) => index.toString()}
        renderItem={({item}) => <Card item={item}/>}
      /> 
-     </ScrollView>
-    </View> 
-     </View>
    );
- } 
-}
+ }
 
+ _get = async (endpoint) => {
+  const res = await fetch(endpoint);
+  const data = await res.json();
+  return data;
+  }
+}
          
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    marginTop:20,
+    backgroundColor: '#F5FCFF',
+  }
+});
+
